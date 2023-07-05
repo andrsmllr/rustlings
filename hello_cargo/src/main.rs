f4d34b6b70b63a93866809df29f7407b01617c13
andrsmllr <andrsmllr@bananatronics.org>
2023-07-05 09:43:38 +0200

    Add progress

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut i = 0;
+    let mut x = 1;
+    let y = if loop {
+        x += x;
+        i += 1;
+        if i == 3 { break x }
+    } == 8 { 1 } else { 0 };
+
+    println!("y = {y}");
 }
